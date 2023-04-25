8e6eb240319a55422393507ec07da0d7bce90ee3
NaferJ <naferjml@gmail.com>
2023-04-25 13:28:12 -0500

    Delete repository innecesary

--- MyDinero/src/main/java/com/example/MyDinero/repositorio/EmpleadoRepositorio.java
+++ MyDinero/src/main/java/com/example/MyDinero/repositorio/EmpleadoRepositorio.java
@@ -1,12 +1,10 @@
 package com.example.MyDinero.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.MyDinero.modelo.Empleado;
 
 
-@Repository
 public interface EmpleadoRepositorio extends JpaRepository<Empleado, Long>{
 
 	
